259af39c9e02b526051a23927996cd20381e9454
LboAnn <extend1994@gmail.com>
2018-03-31 16:26:25 +0800

    Separate ovs running part from installation/initialization

--- lab2_env.sh
+++ lab2_env.sh
@@ -44,15 +44,8 @@ if [ $TARGET == "ovs" ] || [ $TARGET == "all" ];then
   sudo /sbin/modprobe openvswitch
   sudo mkdir -p /usr/local/etc/openvswitch
   sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
-  sudo ovsdb-server \
-    --remote=punix:/usr/local/var/run/openvswitch/db.sock \
-    --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
-    --private-key=db:Open_vSwitch,SSL,private_key \
-    --certificate=db:Open_vSwitch,SSL,certificate \
-    --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
-    --pidfile --detach
-  sudo ovs-vsctl --no-wait init
-  sudo ovs-vswitchd --pidfile --detach
+  # Run up the daemon
+  /bin/bash lab2_ovs.sh
 fi
 
 if [ $TARGET == "ryu" ] || [ $TARGET == "all" ];then
